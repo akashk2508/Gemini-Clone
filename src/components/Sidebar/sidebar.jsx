51c8ee81a795bbc79ebe0ef47f94ee5f15f469c2
Akash <akashk0785@gmail.com>
2026-05-06 12:22:49 +0530

    first commit

---
+++ src/components/Sidebar/sidebar.jsx
@@ -0,0 +1,70 @@
+import React, {useState} from 'react'
+import {assets} from '../../assets/assets'
+import './sidebar.css'
+import { Context } from '../../context/Context'
+import { useContext } from 'react'
+
+
+const Sidebar = () => {
+
+    const[extended, setExtended] = useState(true)
+    const{onSent, prevPrompt, setRecentPrompt, newChat} = useContext(Context);
+
+    const loadPrompt = async (prompt) => {
+      setRecentPrompt(prompt);
+      await onSent(prompt);
+    }
+
+  return (
+    <div className='sidebar'>
+
+      <div className="top">
+        <img onClick={()=>setExtended(prev=>!prev)} src={assets.menu_icon} alt="Icon" className='menu'/>
+
+        <div onClick={()=>newChat()} className="new-chat">
+            <img src={assets.plus_icon} alt="Icon" />
+            {extended ? <p>New Chat</p> : null}
+        </div>
+
+        {extended ? 
+        <div className="recent">
+            <p className="recent-title">Recent</p>
+            
+            {prevPrompt.map((item, index)=>{
+              return(
+                  <div key={index} onClick={()=>loadPrompt(item)} className="recent-entry">
+                    <img src={assets.message_icon} alt="Icon" />
+                    <p>{item.slice(0,18)}...</p>
+
+                  </div>
+              )
+            })}
+
+            
+        </div>
+         : null}
+        
+      </div>
+
+
+
+
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+            <img src={assets.question_icon} alt="Icon" />
+           {extended ? <p>Help</p> : null} 
+        </div>
+        <div className="bottom-item recent-entry">
+            <img src={assets.history_icon} alt="Icon" />
+            {extended ? <p>Activity</p> : null} 
+        </div>
+        <div className="bottom-item recent-entry">
+            <img src={assets.setting_icon} alt="Icon" />
+            {extended ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
